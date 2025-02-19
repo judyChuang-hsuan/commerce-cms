b2caf2280160b8002914ef40fb0141e3d4c19974
Judy Chunag <judychuang24@gmail.com>
2025-02-19 20:40:15 +0800

    feat: 錯誤頁新增返回連結
    錯誤頁新增返回連結

--- src/component/page/error.tsx
+++ src/component/page/error.tsx
@@ -1,5 +1,6 @@
 import { hookInstances, ResetErrorBoundary } from '@/constant'
 import { Button, Result } from 'antd'
+import { useNavigate } from 'react-router-dom'
 
 type ErrorPageProps = {
 	code:
@@ -8,6 +9,7 @@ type ErrorPageProps = {
 }
 
 function BaseError(props: ErrorPageProps) {
+	const navigate = useNavigate()
 	const { error, resetErrorBoundary, code } = props as Partial<{
 		didCatch: boolean
 		error: any
@@ -25,7 +27,11 @@ function BaseError(props: ErrorPageProps) {
 				status={code}
 				title={code}
 				subTitle={error ? error.message : '發生不可預期的錯誤'}
-				extra={<Button type="primary">返回</Button>}
+				extra={
+					<Button type="primary" onClick={() => navigate('/member')}>
+						返回
+					</Button>
+				}
 			/>
 		</div>
 	)
